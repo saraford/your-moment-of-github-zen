4772caba19364f3252be4ca63a33b88bbb19972d
Sara Ford <saraford@users.noreply.github.com>
2016-07-25 11:08:50 -0700

    script now working at top of html

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
-const zenButton = document.getElementById('zen-button');
+'use strict'
 
-zenButton.addEventListener('click', function (event) {
- const url = 'https://api.github.com/zen';
-window.fetch(url, { method: 'get'
- }).then(function(response) {
-   return response.text();
- }).then(function(text) {
-   document.querySelector('#zen-phrase').textContent = text;
- });
-})
+const getZenPhrase = () => {
+  let zenButton = document.getElementById('zen-button');
+
+  zenButton.addEventListener('click', function (event) {
+
+   const url = 'https://api.github.com/zen';
+
+   window.fetch(url, { method: 'get'
+     }).then(function(response) {
+       return response.text();
+     }).then(function(text) {
+       document.querySelector('#zen-phrase').textContent = text;
+     });
+  })
+}
+
+var zenButton = null;
+
+document.addEventListener('DOMContentLoaded', function() {
+  getZenPhrase();
+});
