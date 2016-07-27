d8aa2682da00ce3dee40a48a7fa8952bdddea6cc
Sara Ford <saraford@users.noreply.github.com>
2016-07-27 12:09:45 -0700

    added dotenv

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 'use strict'
+require('dotenv').config();
 
 const getZenPhrase = () => {
   let zenButton = document.getElementById('zen-button');
@@ -6,9 +7,15 @@ const getZenPhrase = () => {
   zenButton.addEventListener('click', function (event) {
 
    const url = 'https://api.github.com/zen';
+   var request = new Request(url, {
+      method: 'GET',
+      headers: new Headers({
+          "User-Agent" : "saraford",
+          "Authorization" : "token " + process.env.TOKEN,
+         })
+    });
 
-   window.fetch(url, { method: 'get'
-     }).then(function(response) {
+     window.fetch(request).then(function(response) {
        return response.text();
      }).then(function(text) {
        document.querySelector('#zen-phrase').textContent = text;
